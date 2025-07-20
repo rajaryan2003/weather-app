a41434ef88e0d0297050fdd967e82c5058e9fe6d
rajaryan2003 <rajaryanfront@gmail.com>
2025-07-20 22:58:31 +0530

    Fix: Added missing dependency to useEffect for Netlify build

--- src/App.js
+++ src/App.js
@@ -1,3 +1,35 @@
+// import "./App.css";
+// import { useEffect } from "react";
+// import Button from "./components/Button";
+// import Input from "./components/Card";
+// import Card from "./components/Input";
+// import { useWeather } from "./context/Weather";
+
+// function App() {
+//   const weather = useWeather();
+//   console.log(weather);
+
+//   useEffect(() => {
+//     // Get Current Location
+//     weather.fetchCurrentUserLocationData();
+//   }, []);
+
+//   return (
+//     <div className="App">
+//       <h1>Weather forecast</h1>
+//       <Input />
+//       <Card />
+//       <Button onClick={weather.fetchData} value="Search" />
+
+//       <Button value="Refresh" />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
 import "./App.css";
 import { useEffect } from "react";
 import Button from "./components/Button";
@@ -6,24 +38,22 @@ import Card from "./components/Input";
 import { useWeather } from "./context/Weather";
 
 function App() {
-  const weather = useWeather();
-  console.log(weather);
+  const { fetchCurrentUserLocationData, fetchData } = useWeather();
 
   useEffect(() => {
-    // Get Current Location
-    weather.fetchCurrentUserLocationData();
-  }, []);
+    fetchCurrentUserLocationData();
+  }, [fetchCurrentUserLocationData]);
 
   return (
     <div className="App">
       <h1>Weather forecast</h1>
       <Input />
       <Card />
-      <Button onClick={weather.fetchData} value="Search" />
-
+      <Button onClick={fetchData} value="Search" />
       <Button value="Refresh" />
     </div>
   );
 }
 
 export default App;
+
